6fc40dc73a7a68ccc4efdf4069697b5c8298ba57
Joe <72690202+joeyouss@users.noreply.github.com>
2021-02-15 16:50:57 +0530

    Add files via upload
    added the starter files

---
+++ starter/script.js
@@ -0,0 +1,79 @@
+'use strict';
+// console.log('hello');
+// REPL.it
+// ES - ES1, ES2, ES3, ES4....... 2015 ES6
+// DETECTS BUGS EASILY
+// PREVENT BUGS FROM HAPPENING
+
+// INT, CHAR, DOUBLE, FLOAT
+// VAR, LET, CONST
+
+// let ok = false;
+// console.log(typeof ok);
+// in JS, DATA TYPES EXIST , BUT WE DO NOT DECLARE THEM SEPARATELY. THE JS ENGINE UNDERSTANDS THE DATA TYPE AUTOMATICALLY
+
+// variable - named storage
+// data can be - a number, a string, boolean
+// 'true', 'false'
+// true, false
+// let msg = 'hello learners';
+// console.log(msg);
+// console.log(typeof msg);
+// let character = 'c';
+// console.log(typeof character);
+// const - means constant - the values you do not want to change
+// var - before ES6 (old school)
+// let - modern way to declare variables
+
+// IN CLASS CODING CHALLENGE 1 -
+// 1. declare 2 variables - admin and name
+// 2. name should have value of 'joe'
+// 3. admin should have value of name
+
+// let admin, name;
+// name = 'joe';
+// admin = name;
+
+// datatypes
+// 1. Number
+// let n = 12.35;
+// let n = 15;
+// let n = 12.87439128319273;
+// special numeric values - Infinity, -Infinity, NaN
+
+// infinity - 1/0
+// -infinity - -1/0
+// NaN - not a number - ('joe'/2)
+
+//  numbers include int, float, double
+
+// 2.BigINt
+// 2^53-1 - this is the limit of nNumber data type
+//  let x = 90071992547409916598486515615648n;
+// bigINt is supported in chrome, safari, firefox but not in IE
+
+// 3. String
+//  3 ways to declare strings
+// "joe"
+// 'joe'
+// `joe`
+
+// 4. Boolean
+//  true or false
+// let n = true;
+// let x = 4 < 1;
+// console.log(x);
+
+// 5. null datatype
+// let x = null;
+
+// 6. undefined
+// let y;
+// console.log(y);
+
+// what is javascript
+// what are variables
+// datatypes
+// strict mode
+// typeof
+// console.log
